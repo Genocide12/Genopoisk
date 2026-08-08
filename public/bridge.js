fcb30d5ef62a7e6e4b7106e3b038be95a9dca62f
Genocide12 <129229824+Genocide12@users.noreply.github.com>
2026-08-08 13:09:35 +0000

    fix: rewrite MPD BaseURL from broken CDN to working CDN
    ROOT CAUSE OF VIDEO NOT PLAYING:
    - MPD manifest (dash+xml) contains <BaseURL> pointing to:
      - https://ghzbfjzbazc.interkh.com/02_24/09/09/A3K3HD33/
      - https://x-bc.interkh.com/02_24/09/09/A3K3HD33/
    - These CDNs return 410 Gone or timeout → venoplayer can't load segments
    - venoplayer combines BaseURL + SegmentTemplate → segment URLs all fail
    - Result: 'No video bytes to push or stream is inactive'
    
    FIX:
    - fetch override: intercept responses with content-type dash+xml or xml,
      parse body, if contains <MPD> replace all ghzbfjzbazc/x-bc URLs with
      hye1eaipby4w (the working CDN that serves the MPD itself)
    - XHR override: same logic via readystatechange listener
    - venoplayer now gets segment URLs like:
      https://hye1eaipby4w.interkh.com/02_24/09/09/A3K3HD33/4.1051568-0/init.webm
    - These should work (same CDN as the manifest)

--- public/bridge.js
+++ public/bridge.js
@@ -49,7 +49,31 @@
         log('P2P CDN blocked (network error):', url.slice(0, 100));
         return Promise.reject(new TypeError('Failed to fetch'));
       }
-      return origFetch.apply(this, arguments);
+      // Intercept MPD manifest responses and rewrite BaseURL from broken
+      // P2P CDNs (ghzbfjzbazc, x-bc) to the working CDN (hye1eaipby4w).
+      // This is the KEY fix for video playback on desktop.
+      return origFetch.apply(this, arguments).then(function(res) {
+        var ct = res.headers.get('content-type') || '';
+        if (ct.indexOf('dash+xml') !== -1 || ct.indexOf('xml') !== -1) {
+          // Clone and rewrite
+          return res.text().then(function(text) {
+            if (text.indexOf('<MPD') === -1) return res;
+            var rewritten = text.replace(
+              /https:\/\/(ghzbfjzbazc|x-bc)\.interkh\.com/g,
+              'https://hye1eaipby4w.interkh.com'
+            );
+            if (rewritten !== text) {
+              log('Rewrote MPD BaseURL: ghzbfjzbazc/x-bc → hye1eaipby4w');
+            }
+            return new Response(rewritten, {
+              status: res.status,
+              statusText: res.statusText,
+              headers: res.headers
+            });
+          });
+        }
+        return res;
+      });
     };
   } catch(e) {}
 
@@ -60,6 +84,7 @@
     XMLHttpRequest.prototype.open = function(method, url) {
       this._genopoiskBlocked = isBlocked(url);
       this._genopoiskP2p = isP2pCdn(url);
+      this._genopoiskUrl = url;
       return origOpen.apply(this, arguments);
     };
     XMLHttpRequest.prototype.send = function(body) {
@@ -92,6 +117,28 @@
         }, 0);
         return;
       }
+      // Intercept MPD manifest responses and rewrite BaseURL
+      var origOnReadyStateChange = this.onreadystatechange;
+      var origOnLoad = this.onload;
+      this.addEventListener('readystatechange', function() {
+        if (self.readyState === 4 && self.status === 200) {
+          var ct = self.getResponseHeader('content-type') || '';
+          if ((ct.indexOf('dash+xml') !== -1 || ct.indexOf('xml') !== -1) &&
+              self.responseText && self.responseText.indexOf('<MPD') !== -1) {
+            var rewritten = self.responseText.replace(
+              /https:\/\/(ghzbfjzbazc|x-bc)\.interkh\.com/g,
+              'https://hye1eaipby4w.interkh.com'
+            );
+            if (rewritten !== self.responseText) {
+              log('Rewrote MPD BaseURL (XHR): ghzbfjzbazc/x-bc → hye1eaipby4w');
+              try {
+                Object.defineProperty(self, 'responseText', { value: rewritten, configurable: true });
+                Object.defineProperty(self, 'response', { value: rewritten, configurable: true });
+              } catch(e) {}
+            }
+          }
+        }
+      });
       return origSend.apply(this, arguments);
     };
   } catch(e) {}
